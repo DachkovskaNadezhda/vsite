b1ded749ef5a8131c1058d96b0f9fe57ecbc5f69
DachkovskaNadezhda <dachkovskanadezhda@gmail.com>
2025-10-31 16:21:19 +0900

    commit

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-fetch('navbar.html')
+fetch('./navbar/navbar.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('navbar-container').innerHTML = data;
     })
     .catch(error => console.error('Error loading navbar:', error));
 
-fetch('./footer.html')
+fetch('./menu/menu.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('menu-container').innerHTML = data;
+    })
+    .catch(error => console.error('Error loading navbar:', error));
+
+fetch('./footer/footer.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('footer-container').innerHTML = data;
