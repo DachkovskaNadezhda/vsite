61436943dbc33d996ce2ce44e39f38e0123f4a78
DachkovskaNadezhda <dachkovskanadezhda@gmail.com>
2025-11-11 16:33:23 +0900

    commit

--- navbar.js
+++ navbar.js
@@ -5,7 +5,27 @@ fetch('./navbar.html')
     })
     .catch(error => console.error('Error loading navbar:', error));
 
+fetch('./menu.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('menu-container').innerHTML = data;
+    })
+    .catch(error => console.error('Error loading navbar:', error));
+
+
+
+function toggleMenuVisibility() {
+    let element = document.getElementById('menu-container');
+    element.classList.toggle('menu-container--visible');
+
+    let searchElement = document.getElementById('search-catalog__input');
+    searchElement.classList.remove('search-catalog__input--visible');
+};
+
 function toggleSearchCatalogInput() {
-    let element =document.getElementById('search-catalog__input');
+    let element = document.getElementById('search-catalog__input');
     element.classList.toggle('search-catalog__input--visible');
-};    
+
+    let menuElement = document.getElementById('menu-container');
+    menuElement.classList.remove('menu-container--visible');
+};  
